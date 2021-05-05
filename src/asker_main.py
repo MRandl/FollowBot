aa1fea618b43268412513b2192ea9e62b9432e9f
Mathis Randl <mathis.randl@epfl.ch>
2021-05-05 13:58:19 +0200

    remove pycache

--- src/asker_main.py
+++ src/asker_main.py
@@ -11,10 +11,10 @@ def get_api():
     os.makedirs(os.path.dirname(password_path), exist_ok=True)
     password_file = open(password_path, 'w')
     key_names = ['oauth login', 'oauth password', 'public access token', 'private access token']
-    keys = [False for x in key_names]
+    keys = []
     for index in range(len(key_names)):
       element = input('\nPlease input your ' + key_names[index] + ':\n')
-      keys[index] = element
+      keys.append(element)
       password_file.write(element + '\n')
     password_file.close()
     print('')
@@ -23,5 +23,18 @@ def get_api():
   auth.set_access_token(keys[2], keys[3])
   return tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 
-def get_list_followings():
-  return ['obama']
+def get_list_followings(api):
+  bfs_path = 'res/init/bfs.txt'
+  if(os.path.exists(bfs_path)):
+    return open(bfs_path, 'r').read().splitlines()
+  
+  else:
+    os.makedirs(os.path.dirname(bfs_path), exist_ok=True)
+    bfs_file = open(bfs_path, 'w')
+    
+    ids_path = input('\nPlease enter the inital ids path : \n')
+    ids = open(ids_path, 'r').read().splitlines()
+    for elem in ids:
+      bfs_file.write(elem + '\n')
+    
+    return ids
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import predictor_main
 # 1
 api = asker_main.get_api()
 # 2
-followings_id = asker_main.get_list_followings()
+followings_id = asker_main.get_list_followings(api)
 print(followings_id)
 # 3
 predictor_main.predict(['BarackObama'])
