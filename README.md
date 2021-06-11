be4a287363af332d0672f54efbe6605644cc6916
Attia Youssef <youssef.attia@epfl.ch>
2021-06-11 13:59:24 +0200

    Final version of code

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# FollowBot
+#Followbot
+##Introduction
+
+Followbot is a proof-of-concept software we developed in the context of our bachelor semester project "Followback prediction" at EPFL in the LSIR lab.
+
+In this project, our goal was to develop a framework that would predict whether a user on Twitter was likely to followback or not, based on a certain number of parameters.
+
+This repository contains a wrapper for the final product of the project; how it works is explained below.
+
+##Requirements
+
+To be able to use the Followbot, you're going to need a few things:
+- Python installed
+- An approved Twitter developer account and the corresponding key, to be able to get information about the users (followers, followings, etc)
+
+#####A Python environment with the following packages:
+
+- Tweepy
+- Pandas
+- Numpy
+- Joblib
+##How it works
+
+Here is a brief explanation of how Followbot proceeds:
+
+Step 1: If it is the first time you use it, the bot will ask you for your developer Twitter API key. This step is crucial as otherwise, you won't be able to use the bot. If you already typed your API key at least once before, this step is skipped.
+
+Step 2: In this step, you either:
+
+are asked to enter a list of screen names of the people you wish to follow; the bot will then decide if those people are likely to followback or not.
+if you followed people using this bot before, it will check whether they followed back or not. If at least one user did, the bot will propose a list of at most 15 people to follow using a Breadth First Search search algorithm; it is up to you to accept or not. If you refuse or if nobody followed you back, you will be asked to type in names once more. Either way, the bot will learn from this experience by fitting the new data.
+
+Step 3: after you are done, you must follow the users manually. Relaunch Followbot after a few days in order to let the potential followbacks happen
+
+
+##How to launch
+
+Simply go to the "src" directory in your terminal and launch the main program with "python main.py" or "python3 main.py". You may need to launch this from an environment with the required packages.
+
+##Authors
+@AttiaYoussef
+
+@MRandl
--- src/prediction/predictor_main.py
+++ src/prediction/predictor_main.py
@@ -1,15 +1,22 @@
 from joblib import dump, load
 import pandas as pd
 import tweepy
+import numpy as np
 import numpy.random as random
 import os.path
 import time
+import pickle
 from prediction.helpers import *
 
 all_user_info = ['id', 'has_chain', 'has_wave', 'has_100', 'has_followback', 'has_corona', 'has_bot',
                  'has_invitation_link', 'last_known_screen_name', 'erate', 'friends', 'followers']
 relevant_info_about_user = ['has_chain', 'has_wave', 'has_100', 'has_followback', 'has_corona', 'has_bot',
                             'has_invitation_link', 'erate', 'friends', 'followers']
+
+mean = np.array(
+    [0.001155, 0.008316, 0.077616, 0.163548, 0.000693, 0.013629, 0.088011, 0.678295, 3146.707323, 3175.801571]).reshape(1, 10)
+std = np.array(
+    [0.033970, 0.090823, 0.267597, 0.369908, 0.026319, 0.115958, 0.283344, 0.301506, 8518.075296, 8639.824506]).reshape(1, 10)
 
 
 def calculateInfoUser(id, api):  # calculate all relevant info of a user; to use for the predictions
@@ -40,14 +47,17 @@ def calculateInfoUser(id, api):  # calculate all relevant info of a user; to use
 
 
 def predict_users_who_followback(ids, api):
-    predictor = load('./prediction/saved.bin')  # get back from file
+    with open('./prediction/model.pkl', 'rb') as f:
+        predictor = pickle.load(f, encoding='utf-8')
     x = pd.DataFrame(columns=all_user_info)
     for id in ids:
         user_info = calculateInfoUser(id, api)
         if user_info is None:
             continue
         x = x.append(pd.Series(user_info, index=all_user_info), ignore_index=True)
-    predicted = predictor.predict(x.drop(axis=1, labels=['id', 'last_known_screen_name']))
+    data = x.drop(axis=1, labels=['id', 'last_known_screen_name'])
+    data = (data.to_numpy() - mean) / std
+    predicted = list(map(lambda x : x[0] < 0.6, predictor.predict_proba(data)))
     x['predicted_followback'] = predicted
     ids_predicted = list(x[predicted].id)
     return x, ids_predicted
@@ -73,6 +83,7 @@ def check_followbacks(api):
             try:
                 user = api.get_user(id)
             except tweepy.TweepError:
+                prediction = prediction[prediction.id != id]
                 continue
             if id in followers:
                 print(user.screen_name + " follows you back")
@@ -81,33 +92,53 @@ def check_followbacks(api):
             else:
                 print(user.screen_name + " does not follows you back")
                 real_followbacks.append(False)
-        predictor = load('./prediction/saved.bin')  # get back from file
-        predictor.partial_fit(prediction.drop(axis = 1, labels=['id', 'last_known_screen_name','predicted_followback']), real_followbacks)
-        dump(predictor, './prediction/saved.bin')
+        with open('./prediction/model.pkl','rb') as f:
+            predictor = pickle.load(f,encoding='utf-8')
+        data = prediction.drop(axis=1, labels=['id', 'last_known_screen_name', 'predicted_followback'])
+        data = (data.to_numpy() - mean) / std
+        for i in range(200):
+            predictor.partial_fit(data,real_followbacks)
+        with open('./prediction/model.pkl','wb') as f:
+            pickle.dump(predictor,f)
         if len(ids_followbacks) > 0:
             try:
-                choice = int(input("If you wish, we can propose to you some users to follow. Type 1 to accept, anything else to refuse : "))
+                choice = int(input(
+                    "If you wish, we can propose to you some users to follow. Type 1 to accept, anything else to refuse : "))
             except ValueError:
                 return None
             if choice == 1:
-                print("We will propose to you at most users, found using a BFS method (this may take some time) ")
+                print("We will propose to you at most 15 users, found using a BFS method (this may take some time). Some will be random; others will have a good followers/followings ratio ")
                 id_user = ids_followbacks[0]
                 followers = list(
                     tweepy.Cursor(api.followers_ids, id=id_user, wait_on_rate_limit=True,
                                   wait_on_rate_limit_notify=True).items(500))
                 people_to_try = []
-                for follower_id in random.choice(followers, 15):
+                for follower_id in random.choice(followers, 7):
                     try:
-                        user = api.get_user(follower_id) #to verify that the user still exists
+                        user = api.get_user(follower_id)  # to verify that the user still exists
                     except tweepy.TweepError:
                         continue
                     people_to_try.append(follower_id)
+                count = 8
+                for follower_id in followers:
+                    if count == 0:
+                        break
+                    if follower_id in people_to_try:
+                        continue
+                    else:
+                        try:
+                            user = api.get_user(follower_id)
+                        except tweepy.TweepError:
+                            continue
+                        if user.friends_count == 0:
+                            continue
+                        ratio = user.followers_count / user.friends_count
+                        if ratio >= 0.90 and ratio <= 1:
+                            count -= 1
+                            people_to_try.append(follower_id)
+
                 return people_to_try
             else:
                 return None
         else:
             return None
-
-
-
-
