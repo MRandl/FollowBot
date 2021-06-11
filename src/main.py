19c29c67d963a893938aac001c23eadadddb3fe4
Attia Youssef <youssef.attia@epfl.ch>
2021-06-11 09:43:16 +0200

    Changed main

--- src/main.py
+++ src/main.py
@@ -9,17 +9,16 @@ def ask_followings(potential_advised_followings = None):
     else:
         followings_id = potential_advised_followings
     df, potential_followers = predictor_main.predict_users_who_followback(followings_id, api)
-    if len(potential_followers) == 0:
-        print("No one was predicted to follow you back; try again with other names")
-        ask_followings()
-    else:
-        print("Users we believe are likely to followack: ")
+    if len(potential_followers) > 0:
+        print("Users we believe are likely to followback: ")
         for id in potential_followers:
             user = api.get_user(id)
             print(user.screen_name)
-        print("You can always try following the others, we simply don't think they will followback.")
-        print("All users are in the prediction.csv file in the res directory.")
-        df.to_csv("./res/prediction.csv", index=False)
+    else:
+        print("No user was predicted to followback")
+    print("You can try following everyone, we simply don't think they will all followback.")
+    print("All users are in the prediction.csv file in the res directory.")
+    df.to_csv("./res/prediction.csv", index=False)
 
 print("Welcome to Followbot")
 # 1
